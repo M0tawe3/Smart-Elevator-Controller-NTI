916f7021573738e5bde0a354a843e5fb29e9e90a
Your Name <ys13101310@gmail.com>
2026-09-14 20:26:33 +0300

    Add Day 2 slot sensor GPIO input test

--- AVR_NTI/main.c
+++ AVR_NTI/main.c
@@ -15,14 +15,35 @@
 #include "ADC_interface.h"
 #include "TIMER_interface.h"
 #include "UART_interface.h"
+#include "HAL/Slots/slots.h"
 
 int main(void)
 {
-  UART_Init(9600);
+    uint8 sensor_mask = 0u;
 
-  while(1){
-    UART_SendString("Hello World!");
-    TIMER0_DelayMS(2000);
-  }
-  return 0;
+    UART_Init(9600);
+    slots_init();
+
+    UART_SendString("Slot sensor test started\r\n");
+
+    while (1)
+    {
+        if (slots_read_raw(&sensor_mask) == E_OK)
+        {
+            UART_SendString("Sensors: ");
+
+            for (uint8 bit = SLOT_SENSOR_START_PIN; bit <= SLOT_SENSOR_END_PIN; bit++)
+            {
+                uint8 value = (sensor_mask >> bit) & 1u;
+                UART_SendByte((uint8)('0' + value));
+                UART_SendByte(' ');
+            }
+
+            UART_SendString("\r\n");
+        }
+
+        TIMER0_DelayMS(100);
+    }
+
+    return 0;
 }
